fd67af5cdaf4cebea8c40d623b792f2bdd19c8ea
crimson-carnival <etherealcarnival@gmail.com>
2020-08-30 17:24:46 +0530

    1. Corrected installation instructions in README.md 2. Updated dependencies

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 ## About
 **Knowledge-based Disease Prediction System** is a Python based web application that can be used to predict diseases based on symptoms.
 
-*Work is still under progress.*
-
 ## Technologies used:
-* Python
-    * Flask
-    * Flask-PyMongo
-    * PyMongo
+* Python - Flask
 * MongoDB
 
-## Dependency Installation
-Please install the following, in order to use the application:
-1. `sudo pip install pymongo`
-2. `sudo pip install flask_pymongo`
-
-*Make sure MongoDB in installed on the system that this application will run on.*
+## Dependencies:
+1. Install the dependencies from requirements.txt:
+    `pip install -r -requirements.txt`
+2. MongoDB, on port `27017`
 
 # How to run:
 1) Execute the file 'app.py' using `python app.py` in order to start the application.
--- app.py
+++ app.py
@@ -1,81 +1,94 @@
-from flask import Flask, request, render_template, redirect, session, Session, send_file
+from flask import Flask, request, render_template, redirect, session, send_file
 from flask_pymongo import PyMongo
 import numpy as np
 import pandas as pd
 
 # Machine learning stuff
 
-l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-'yellow_crust_ooze']
+l1 = ['back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine',
+      'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach',
+      'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation',
+      'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs',
+      'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool',
+      'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs',
+      'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails',
+      'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips',
+      'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints',
+      'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness',
+      'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine',
+      'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)',
+      'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain',
+      'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria',
+      'family_history', 'mucoid_sputum',
+      'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion',
+      'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen',
+      'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf',
+      'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling',
+      'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose',
+      'yellow_crust_ooze']
 
-disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
-'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
-' Migraine','Cervical spondylosis',
-'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
-'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
-'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
-'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
-'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
-'Impetigo']
+disease = ['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction',
+           'Peptic ulcer diseae', 'AIDS', 'Diabetes', 'Gastroenteritis', 'Bronchial Asthma', 'Hypertension',
+           ' Migraine', 'Cervical spondylosis',
+           'Paralysis (brain hemorrhage)', 'Jaundice', 'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A',
+           'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E', 'Alcoholic hepatitis', 'Tuberculosis',
+           'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
+           'Heartattack', 'Varicoseveins', 'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
+           'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis',
+           'Impetigo']
 
-l2=[]
-for x in range(0,len(l1)):
+l2 = []
+for x in range(0, len(l1)):
     l2.append(0)
 
 # TESTING DATA df -------------------------------------------------------------------------------------
-df=pd.read_csv("Training.csv")
+df = pd.read_csv("Training.csv")
 
-df.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
-'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
-'Migraine':11,'Cervical spondylosis':12,
-'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
-'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
-'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
-'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
-'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
-'Impetigo':40}},inplace=True)
+df.replace({'prognosis': {'Fungal infection': 0, 'Allergy': 1, 'GERD': 2, 'Chronic cholestasis': 3, 'Drug Reaction': 4,
+                          'Peptic ulcer diseae': 5, 'AIDS': 6, 'Diabetes ': 7, 'Gastroenteritis': 8,
+                          'Bronchial Asthma': 9, 'Hypertension ': 10,
+                          'Migraine': 11, 'Cervical spondylosis': 12,
+                          'Paralysis (brain hemorrhage)': 13, 'Jaundice': 14, 'Malaria': 15, 'Chicken pox': 16,
+                          'Dengue': 17, 'Typhoid': 18, 'hepatitis A': 19,
+                          'Hepatitis B': 20, 'Hepatitis C': 21, 'Hepatitis D': 22, 'Hepatitis E': 23,
+                          'Alcoholic hepatitis': 24, 'Tuberculosis': 25,
+                          'Common Cold': 26, 'Pneumonia': 27, 'Dimorphic hemmorhoids(piles)': 28, 'Heart attack': 29,
+                          'Varicose veins': 30, 'Hypothyroidism': 31,
+                          'Hyperthyroidism': 32, 'Hypoglycemia': 33, 'Osteoarthristis': 34, 'Arthritis': 35,
+                          '(vertigo) Paroymsal  Positional Vertigo': 36, 'Acne': 37, 'Urinary tract infection': 38,
+                          'Psoriasis': 39,
+                          'Impetigo': 40}}, inplace=True)
 
 # print(df.head())
 
-X= df[l1]
+X = df[l1]
 
 y = df[["prognosis"]]
 np.ravel(y)
 # print(y)
 
 # TRAINING DATA tr --------------------------------------------------------------------------------
-tr=pd.read_csv("Testing.csv")
-tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
-'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
-'Migraine':11,'Cervical spondylosis':12,
-'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
-'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
-'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
-'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
-'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
-'Impetigo':40}},inplace=True)
+tr = pd.read_csv("Testing.csv")
+tr.replace({'prognosis': {'Fungal infection': 0, 'Allergy': 1, 'GERD': 2, 'Chronic cholestasis': 3, 'Drug Reaction': 4,
+                          'Peptic ulcer diseae': 5, 'AIDS': 6, 'Diabetes ': 7, 'Gastroenteritis': 8,
+                          'Bronchial Asthma': 9, 'Hypertension ': 10,
+                          'Migraine': 11, 'Cervical spondylosis': 12,
+                          'Paralysis (brain hemorrhage)': 13, 'Jaundice': 14, 'Malaria': 15, 'Chicken pox': 16,
+                          'Dengue': 17, 'Typhoid': 18, 'hepatitis A': 19,
+                          'Hepatitis B': 20, 'Hepatitis C': 21, 'Hepatitis D': 22, 'Hepatitis E': 23,
+                          'Alcoholic hepatitis': 24, 'Tuberculosis': 25,
+                          'Common Cold': 26, 'Pneumonia': 27, 'Dimorphic hemmorhoids(piles)': 28, 'Heart attack': 29,
+                          'Varicose veins': 30, 'Hypothyroidism': 31,
+                          'Hyperthyroidism': 32, 'Hypoglycemia': 33, 'Osteoarthristis': 34, 'Arthritis': 35,
+                          '(vertigo) Paroymsal  Positional Vertigo': 36, 'Acne': 37, 'Urinary tract infection': 38,
+                          'Psoriasis': 39,
+                          'Impetigo': 40}}, inplace=True)
 
-X_test= tr[l1]
+X_test = tr[l1]
 y_test = tr[["prognosis"]]
 np.ravel(y_test)
+
+
 # ------------------------------------------------------------------------------------------------------
 
 
@@ -86,12 +99,11 @@ class Diagnosis:
 
 
 def decision_tree(symptoms):
-
     diagnosis = Diagnosis()
 
     from sklearn import tree
 
-    clf3 = tree.DecisionTreeClassifier()   # empty model of the decision tree
+    clf3 = tree.DecisionTreeClassifier()  # empty model of the decision tree
     clf3 = clf3.fit(X, y)
 
     # calculating accuracy-------------------------------------------------------------------
@@ -103,7 +115,7 @@ def decision_tree(symptoms):
 
     p_symptoms = [symptoms[0], symptoms[1], symptoms[2], symptoms[3], symptoms[4]]
 
-    for k in range(0,len(l1)):
+    for k in range(0, len(l1)):
         # print (k,)
         for z in p_symptoms:
             if z == l1[k]:
@@ -132,7 +144,6 @@ def decision_tree(symptoms):
 
 
 def random_forest(symptoms):
-
     diagnosis = Diagnosis()
 
     from sklearn.ensemble import RandomForestClassifier
@@ -148,10 +159,10 @@ def random_forest(symptoms):
 
     p_symptoms = [symptoms[0], symptoms[1], symptoms[2], symptoms[3], symptoms[4]]
 
-    for k in range(0,len(l1)):
+    for k in range(0, len(l1)):
         for z in p_symptoms:
             if z == l1[k]:
-                l2[k]=1
+                l2[k] = 1
 
     input_test = [l2]
     predict = clf4.predict(input_test)
@@ -176,7 +187,6 @@ def random_forest(symptoms):
 
 
 def naive_bayes(symptoms):
-
     diagnosis = Diagnosis()
 
     from sklearn.naive_bayes import GaussianNB
@@ -191,7 +201,7 @@ def naive_bayes(symptoms):
     # # -----------------------------------------------------
 
     p_symptoms = [symptoms[0], symptoms[1], symptoms[2], symptoms[3], symptoms[4]]
-    for k in range(0,len(l1)):
+    for k in range(0, len(l1)):
         for z in p_symptoms:
             if z == l1[k]:
                 l2[k] = 1
@@ -217,6 +227,7 @@ def naive_bayes(symptoms):
 
     return diagnosis
 
+
 # Machine learning stuff ends
 
 # Flask stuff
@@ -225,7 +236,6 @@ def naive_bayes(symptoms):
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super_secret_key'
-session = Session()
 app.config["MONGO_URI"] = "mongodb://localhost:27017/KbDPES"
 
 mongo = PyMongo(app)
@@ -268,7 +278,7 @@ def registered():
     age = request.form['age']
     gender = request.form['gender']
     users.insert({'username': username, 'password': password, 'fname': first_name, 'lname': last_name,
-              'age': age, 'gender': gender})
+                  'age': age, 'gender': gender})
     session.pop('temp_username', None)
     session.pop('temp_password', None)
     session['username'] = username
